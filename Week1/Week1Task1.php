f0371044b7e3433aa70b946e057357dc7ef19653
Michael <malopez1@email.neit.edu>
2021-03-30 11:58:49 -0400

    minor tweaks

---
+++ Week1/Week1Task1.php
@@ -0,0 +1,17 @@
+<?php 
+    
+    $animals = [
+        'Leopard Geckos',
+        'Chickens',
+        'Cats',
+        'Cows'
+    ];
+    
+
+    
+    foreach($aninmals as $animal){
+        echo $animal;
+    }
+
+    
+?>
