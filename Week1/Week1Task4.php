18b2b533958f1f47b842e719e83ee102d1d1e3a7
Michael <malopez1@email.neit.edu>
2021-04-03 15:26:47 -0400

    Task 4 Completed

---
+++ Week1/Week1Task4.php
@@ -0,0 +1,29 @@
+<?php
+
+
+require 'functions.php';
+
+//$animals = ['dog','cat'];
+
+//dd('hello world');
+
+//dd($animals);
+
+//require 'index.view.php';
+
+
+
+//**************************************************** */
+// ASSIGNMENT 4 - NIGHTCLUB ENTRY
+
+$olderPerson = 33;
+$person = 18;
+
+//Here i have two differnet uses of the function with different ages
+//Younger person who will fail the entry of the club
+echo ageCheck($person);
+
+//Older person who will be told top enjoy the club
+//echo ageCheck($olderPerson);
+
+
