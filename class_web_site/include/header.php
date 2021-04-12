ec88fbbe533306d6d3ab3fc893ad4e369f276c2b
Michael <malopez1@email.neit.edu>
2021-04-12 11:01:00 -0400

    mino fixes

--- class_web_site/include/header.php
+++ class_web_site/include/header.php
@@ -3,7 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <title>SE 266 - Erik van Renselaar</title>
+  <title>SE 266 - Michael Lopez</title>
 <style>
 
 body {
