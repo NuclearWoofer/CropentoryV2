a2cf7c6406073b3d2ea52d78d928449aea99788d
Michael <malopez1@email.neit.edu>
2021-04-17 13:43:52 -0400

    fizzBuzz Fix

--- class_web_site/Week1/fizzBuzz.php
+++ class_web_site/Week1/fizzBuzz.php
@@ -2,26 +2,30 @@
 
 <?php
 
-//A foreach loop that goes between 1 and 100.
-foreach(range(1, 100) as $number){
+
+
+function fizzBuzz($num)
+{
+    if ($num % 3 === 0) {
+        return ("Fizz");
+    }
     
-    
-    //If the number is divisible by both 3 and 5, then it is Fizz Buzz.
-    if($number % 3 == 0 && $number % 5 == 0){
-        echo 'Fizz Buzz', '<br>';
-    } 
-    //If the number is divisible by 3, then it is Fizz.
-    elseif($number % 3 == 0){
-        echo 'Fizz', '<br>';
+    if ($num % 5 === 0){
+        return ("Buzz");
     }
-    //If the number is divisible by 5, then it is Buzz.
-    elseif($number % 5 == 0){
-        echo 'Buzz', '<br>';
+
+    if ($num % 3 === 0 && $num % 5 === 0){
+        return ("FizzBuzz");
     }
-    //If it isn't divisible by 3 or 5, just print out the number.
-    else{
-        echo $number, '<br>';
-    }
+
+    return ($num);
 }
+
+for ($i=1; $i<=100; $i++) {
+    echo fizzBuzz($i) . "<br />";
+}
+
+
+
 
 include __DIR__ . '/../include/footer.php'; ?>
