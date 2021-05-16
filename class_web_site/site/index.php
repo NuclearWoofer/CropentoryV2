741e119bae9f2181bb554d446e105faca6ddd559
Michael <malopez1@email.neit.edu>
2021-05-16 19:42:06 -0400

    updated home screen

--- class_web_site/site/index.php
+++ class_web_site/site/index.php
@@ -13,7 +13,7 @@ Welcome to my PHP page. You can find an overview of all my working PHP projects 
     <li><a href="../week3/index.php">Week 3 - ATM </a></li>
     <li><a href="../week4/patientView.php">Week 4 - Adding and Viewing Patients from a Database</a></li>
     <li><a href="../week4/patientView.php">Week 5 - Deleting and Updating patient records from a Database</a></li>
-    <li><a href="../week4/patientView.php">Patient Metrics</a></li>
+    <li><a href="../week4/patientView.php">Week 6 - Patient Metrics</a></li>
     <li><a href="../week7/index.php">Week 7</a></li>
     <li><a href="../week8/index.php">Week 8</a></li>
     <li><a href="../week9/index.php">Week 9</a></li>
