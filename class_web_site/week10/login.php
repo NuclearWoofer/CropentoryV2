081d9391b1a34f6080b3395a3bef25d1bfaab011
Michael <malopez1@email.neit.edu>
2021-09-20 16:57:07 -0400

    add

--- class_web_site/week10/login.php
+++ class_web_site/week10/login.php
@@ -42,7 +42,7 @@
         #mainDiv {font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;} 
         .col1 {width: 100px; }
         /*.col2 {float: left;} */
-        .rowContainer {clear: none; height: 40px; width: 500px;}  
+        .rowContainer {height: 40px; width: 500px;}  
         .error {margin-left: 100px; clear: left; color: red;}
 
         .flex-container {
