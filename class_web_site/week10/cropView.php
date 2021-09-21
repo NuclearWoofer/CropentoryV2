2f5add281fcf360bb1555c3bd41a878b4a026d91
Michael <malopez1@email.neit.edu>
2021-09-20 21:12:14 -0400

    phase out button

--- class_web_site/week10/cropView.php
+++ class_web_site/week10/cropView.php
@@ -130,7 +130,7 @@
 
         <div class="form-group">        
             <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">Plant this Plant!</button>
+            <!--<button type="submit" class="btn btn-default">Plant this Plant!</button>-->
             <?php
                 if (isPostRequest()) {
                   echo "Plant successfully Planted!";
