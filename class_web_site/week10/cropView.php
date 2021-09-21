38c4b27ea32eecc1293fae0f894c346d0e2f14d3
Michael <malopez1@email.neit.edu>
2021-09-20 20:36:29 -0400

    pagination test

--- class_web_site/week10/cropView.php
+++ class_web_site/week10/cropView.php
@@ -123,6 +123,15 @@
    
   
     <table class="table table-striped">
+        <script>
+            // Basic example
+            $(document).ready(function () {
+                 $('#dtBasicExample').DataTable({
+                "pagingType": "simple" // "simple" option for 'Previous' and 'Next' buttons only
+             });
+             $('.dataTables_length').addClass('bs-select');
+            });
+        </script>
             <thead>
                 <tr>
 
